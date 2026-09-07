95a4a196f7faf8abc09e3d4234d05bc43dde2772
Codet Code <lohmaeli@huhs.org>
2026-09-07 18:02:34 -0500

    yes

--- sw.js
+++
@@ -1,8 +0,0 @@
-// Service worker skeleton to satisfy PWA install requirements
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
-});
